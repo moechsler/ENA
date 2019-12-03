592a22ee72ce9650347cc1f61d8452216da4c184
MARCO LOTARIO OECHSLER CENTENO <MARCO.OECHSLER@correoaiep.cl>
2019-12-02 22:54:15 -0300

    primer commit

---
+++ ENA/login.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="login.aspx.cs" Inherits="ENA.login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+           <table>
+               <tr><th>Taller de aplicaciones para internet en .NET</th></tr>
+              <tr><td>Autenticacion </td></tr>
+               <tr><td><asp:Label ID="Label1" runat="server" Text="Label">Usuario: </asp:Label></td><td>
+                   <asp:TextBox ID="TextUsuario" runat="server"></asp:TextBox></td></tr>
+                 <tr><td>
+                     <asp:Label ID="Label2" runat="server" Text="Label">Password: </asp:Label></td><td>
+                         <asp:TextBox ID="TestPassword" runat="server" TextMode="Password"></asp:TextBox></td></tr>
+               <tr><td colspan="2">
+                   <asp:Button ID="BtIngresar" runat="server" Text="Button" />
+                   </td></tr>
+           </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ENA/login.aspx.designer.cs
@@ -0,0 +1,69 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace ENA {
+    
+    
+    public partial class login {
+        
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// Control Label1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+        
+        /// <summary>
+        /// Control TextUsuario.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextUsuario;
+        
+        /// <summary>
+        /// Control Label2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label2;
+        
+        /// <summary>
+        /// Control TestPassword.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TestPassword;
+        
+        /// <summary>
+        /// Control BtIngresar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BtIngresar;
+    }
+}
